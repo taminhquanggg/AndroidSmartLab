608647ab4eabf4ad615dc5540c01eb1114431222
Tạ Minh Quang <taquang0211@gmail.com>
2023-12-02 23:55:10 +0700

    init code

---
+++ app/src/main/java/com/example/smartlab/Loading.java
@@ -0,0 +1,17 @@
+package com.example.smartlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.style.Circle;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
